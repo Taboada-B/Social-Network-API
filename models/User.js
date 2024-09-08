0d8e5878c3d603625f54390305c4ef11c9135d4b
Bryan Taboada <b.taboada123@gmail.com>
2024-09-08 12:42:25 -0600

    setting up models, starting routes

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+// const thoughtsSchema = require('./Thought');
+// const reactionSchema = require('./Reaction');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            max_length: 20,
+        },
+
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match a valid email address'],
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+
+module.exports = User;
